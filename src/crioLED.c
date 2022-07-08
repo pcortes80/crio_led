7c5c85db7aa0d53ac9313633e98a4873c1ea82cf
Patricio Cortes <patricio.cortes@noirlab.edu>
2022-07-08 10:28:54 -0400

    Remove NiFpga_MergeStatus

--- src/crioLED.c
+++ src/crioLED.c
@@ -1,7 +1,7 @@
 /*
  * FPGA Interface C API example for GCC.
  * Demo for cRIO-9033 with NI-9344 module
- * Driving User LED0 andUserLED1
+ * Driving User LED0 to LED2
  */
 
 #include "../include/NiFpga_mainFPGA.h"
@@ -19,56 +19,58 @@ int main()
       NiFpga_Session session;
       /* opens a session and downloads the bitstream */
       printf("Opening a session...\n");
-      NiFpga_MergeStatus(&status, NiFpga_Open(NiFpga_mainFPGA_Bitfile,
-                                              NiFpga_mainFPGA_Signature,
-                                              "RIO0",
-                                              NiFpga_OpenAttribute_NoRun,
-                                              &session));
+      //NiFpga_MergeStatus(&status);
+      NiFpga_Open(NiFpga_mainFPGA_Bitfile,
+                                        NiFpga_mainFPGA_Signature,
+                                        "RIO0",
+                                        NiFpga_OpenAttribute_NoRun,
+                                        &session);
       if (NiFpga_IsNotError(status))
       {
          /* Reset LEDs */
-         NiFpga_MergeStatus(&status, NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_LED0,0));
-         NiFpga_MergeStatus(&status, NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_LED1,0));
-         NiFpga_MergeStatus(&status, NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_LED2,0));
-         NiFpga_MergeStatus(&status, NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_LED3,0));
+         NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_LED0,0);
+         NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_LED1,0);
+         NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_LED2,0);
+         NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_LED3,0);
          
          /* run the FPGA application */
          printf("Running the FPGA...\n");
-         NiFpga_MergeStatus(&status, NiFpga_Run(session, 0));
+         NiFpga_Run(session, 0);
          
          /* Select option 0 */          
-         NiFpga_MergeStatus(&status, NiFpga_WriteI16(session, NiFpga_mainFPGA_ControlI16_options, 0));
+         NiFpga_WriteI16(session, NiFpga_mainFPGA_ControlI16_options, 0);
          printf("Selected option 0.\n");
 
          /* Update LEDs individualy */          
-         NiFpga_MergeStatus(&status, NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_LED0,1));
+         NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_LED0,1);
          sleep(2);
-         NiFpga_MergeStatus(&status, NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_LED1,1));
+         NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_LED1,1);
          sleep(2);
-         NiFpga_MergeStatus(&status, NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_LED2,1));
+         NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_LED2,1);
          sleep(2);
-         NiFpga_MergeStatus(&status, NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_LED3,1));
+         NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_LED3,1);
          sleep(2);
 
          /* Select option 1 */          
-         NiFpga_MergeStatus(&status, NiFpga_WriteI16(session, NiFpga_mainFPGA_ControlI16_options, 1));
+         NiFpga_WriteI16(session, NiFpga_mainFPGA_ControlI16_options, 1);
          printf("Selected option 1.\n");
          
          /* Update LEDs from integerToBoolArray U8 */
          uint8_t i;
          for (i = 0; i <= 15; i++) {
-            NiFpga_MergeStatus(&status, NiFpga_WriteU8(session, NiFpga_mainFPGA_ControlU8_integerToBoolArray, i));
+            NiFpga_WriteU8(session, NiFpga_mainFPGA_ControlU8_integerToBoolArray, i);
             printf("i = %d\n", i);
             sleep(1);
          }
 
          /* Select option 2 */          
-         NiFpga_MergeStatus(&status, NiFpga_WriteI16(session, NiFpga_mainFPGA_ControlI16_options, 2));
+         NiFpga_WriteI16(session, NiFpga_mainFPGA_ControlI16_options, 2);
          printf("Selected option 2.\n");
 
          /* Update LEDs from integerInputs U8 */
+         NiFpga_WriteU8(session, NiFpga_mainFPGA_ControlU8_integerInputs, i);
          for (i = 0; i <= 15; i++) {
-            NiFpga_MergeStatus(&status, NiFpga_WriteU8(session, NiFpga_mainFPGA_ControlU8_integerInputs, i));
+            NiFpga_WriteU8(session, NiFpga_mainFPGA_ControlU8_integerToBoolArray, i);
             printf("i = %d\n", i);
             sleep(1);
          }
@@ -80,11 +82,11 @@ int main()
          
          /* close the session now that we're done */
          printf("Closing the session...\n");
-         NiFpga_MergeStatus(&status, NiFpga_Close(session, 0));
+         NiFpga_Close(session, 0);
       }
       /* must be called after all other calls */
       printf("Finalizing...\n");
-      NiFpga_MergeStatus(&status, NiFpga_Finalize());
+      NiFpga_Finalize();
    }
    /* check if anything went wrong */
    if (NiFpga_IsError(status))
